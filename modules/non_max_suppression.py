8dfcd7fb62453cacf8db7a2a4447baf9adb0e069
RAGHVENDRA PAL <35388828+RaghvendraPal@users.noreply.github.com>
2020-05-22 13:13:09 +0530

    Add files via upload

---
+++ modules/non_max_suppression.py
@@ -0,0 +1,142 @@
+import numpy as np
+import math
+
+def non_max_suppression_fast(boxes, probs, overlap_thresh=0.9, max_boxes=300):
+	# code used from here: http://www.pyimagesearch.com/2015/02/16/faster-non-maximum-suppression-python/
+	# if there are no boxes, return an empty list
+  
+    # Process explanation:
+    #   Step 1: Sort the probs list
+    #   Step 2: Find the larget prob 'Last' in the list and save it to the pick list
+    #   Step 3: Calculate the IoU with 'Last' box and other boxes in the list. If the IoU is larger than overlap_threshold, delete the box from list
+    #   Step 4: Repeat step 2 and step 3 until there is no item in the probs list 
+	if len(boxes) == 0:
+		return []
+
+	# grab the coordinates of the bounding boxes
+	x1 = boxes[:, 0]
+	y1 = boxes[:, 1]
+	x2 = boxes[:, 2]
+	y2 = boxes[:, 3]
+
+	np.testing.assert_array_less(x1, x2)
+	np.testing.assert_array_less(y1, y2)
+
+	# if the bounding boxes integers, convert them to floats --
+	# this is important since we'll be doing a bunch of divisions
+	if boxes.dtype.kind == "i":
+		boxes = boxes.astype("float")
+
+	# initialize the list of picked indexes	
+	pick = []
+
+	# calculate the areas
+	area = (x2 - x1) * (y2 - y1)
+
+	# sort the bounding boxes 
+	idxs = np.argsort(probs)
+
+	# keep looping while some indexes still remain in the indexes
+	# list
+	while len(idxs) > 0:
+		# grab the last index in the indexes list and add the
+		# index value to the list of picked indexes
+		last = len(idxs) - 1
+		i = idxs[last]
+		pick.append(i)
+
+		# find the intersection
+
+		xx1_int = np.maximum(x1[i], x1[idxs[:last]])
+		yy1_int = np.maximum(y1[i], y1[idxs[:last]])
+		xx2_int = np.minimum(x2[i], x2[idxs[:last]])
+		yy2_int = np.minimum(y2[i], y2[idxs[:last]])
+
+		ww_int = np.maximum(0, xx2_int - xx1_int)
+		hh_int = np.maximum(0, yy2_int - yy1_int)
+
+		area_int = ww_int * hh_int
+
+		# find the union
+		area_union = area[i] + area[idxs[:last]] - area_int
+
+		# compute the ratio of overlap
+		overlap = area_int/(area_union + 1e-6)
+
+		# delete all indexes from the index list that have
+		idxs = np.delete(idxs, np.concatenate(([last],
+			np.where(overlap > overlap_thresh)[0])))
+
+		if len(pick) >= max_boxes:
+			break
+
+	# return only the bounding boxes that were picked using the integer data type
+	boxes = boxes[pick].astype("int")
+	probs = probs[pick]
+	return boxes, probs
+
+def apply_regr_np(X, T):
+	"""Apply regression layer to all anchors in one feature map
+
+	Args:
+		X: shape=(4, 18, 25) the current anchor type for all points in the feature map
+		T: regression layer shape=(4, 18, 25)
+
+	Returns:
+		X: regressed position and size for current anchor
+	"""
+	try:
+		x = X[0, :, :]
+		y = X[1, :, :]
+		w = X[2, :, :]
+		h = X[3, :, :]
+
+		tx = T[0, :, :]
+		ty = T[1, :, :]
+		tw = T[2, :, :]
+		th = T[3, :, :]
+
+		cx = x + w/2.
+		cy = y + h/2.
+		cx1 = tx * w + cx
+		cy1 = ty * h + cy
+
+		w1 = np.exp(tw.astype(np.float64)) * w
+		h1 = np.exp(th.astype(np.float64)) * h
+		x1 = cx1 - w1/2.
+		y1 = cy1 - h1/2.
+
+		x1 = np.round(x1)
+		y1 = np.round(y1)
+		w1 = np.round(w1)
+		h1 = np.round(h1)
+		return np.stack([x1, y1, w1, h1])
+	except Exception as e:
+		print(e)
+		return X
+    
+def apply_regr(x, y, w, h, tx, ty, tw, th):
+    # Apply regression to x, y, w and h
+	try:
+		cx = x + w/2.
+		cy = y + h/2.
+		cx1 = tx * w + cx
+		cy1 = ty * h + cy
+		w1 = math.exp(tw) * w
+		h1 = math.exp(th) * h
+		x1 = cx1 - w1/2.
+		y1 = cy1 - h1/2.
+		x1 = int(round(x1))
+		y1 = int(round(y1))
+		w1 = int(round(w1))
+		h1 = int(round(h1))
+
+		return x1, y1, w1, h1
+
+	except ValueError:
+		return x, y, w, h
+	except OverflowError:
+		return x, y, w, h
+	except Exception as e:
+		print(e)
+		return x, y, w, h
